38033cc55bb33db0b3fada2c6d8a61f0158065cb
ul <ul7610@pm.me>
2022-06-12 19:13:39 +0200

    add tox.ini for flake

--- AUI_zipcode_modifications_0.85610.py
+++ AUI_zipcode_modifications_0.85610.py
@@ -14,16 +14,15 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from sklearn.ensemble import StackingRegressor
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression, Lasso
+
+# from sklearn.metrics import r2_score
 from sklearn.model_selection import (
-    cross_val_score,
     GridSearchCV,
-    RepeatedKFold,
     train_test_split,
 )
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 CORR_THRES = 0.8
 N = 4
